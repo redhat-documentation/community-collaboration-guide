aca288bcde1973d5bfee5ba336945c55291e625b
Andrew Dahms <adahms@redhat.com>
2017-08-25 14:50:05 +1000

    Updated the sync script to use a batch copy to get the required files.

--- fetch-upstream.sh
+++ fetch-upstream.sh
@@ -9,7 +9,11 @@ echo
 curl -L -o $branch.zip https://github.com/redhat-documentation/community-contribution-template/archive/$branch.zip
 
 # -o = overwrite
-unzip -o $branch.zip community-contribution-template-$branch/doc-Community_Collaboration_Guide/* -d doc-Community_Collaboration_Guide
+unzip -o $branch.zip community-contribution-template-$branch/*
+
+cp -r community-contribution-template-$branch/* .
+
+rm -rf community-contribution-template-$branch
 
 rm $branch.zip
 
